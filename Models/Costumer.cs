ff7ec6e127e15953d70c47559aadf965836a1093
Orlando <ato.murcio@gmail.com>
2021-06-04 15:55:21 -0600

    Fix typo

--- Models/Costumer.cs
+++ Models/Costumer.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Vidly.Models
 {
-    public class Costumer
+    public class Customer
     {
-        public  int Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
