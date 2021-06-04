3d6cfdc2fc9afe57cfb1c7c2e908d5e731b2b7dc
Orlando <ato.murcio@gmail.com>
2021-06-04 15:54:50 -0600

    Define new action to get movies

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,62 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
-
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-
-            var customers = new List<Costumer>
+            var customers = new List<Customer>
             {
-                new Costumer { Name = "Customer1" },
-                new Costumer { Name = "Customer2"}
-
+                new Customer { Id=1, Name = "Customer 1" },
+                new Customer { Id=2,  Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
-                Costumers = customers
+                Customers = customers
             };
-
 
             return View(viewModel);
         }
-        //GET: movies/released/{year}/{month}
-        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-
-        //movies/edit/id
-        public ActionResult Edit(int id)
-        {
-            return Content("the Id you selected is " + id);
-        }
-
-        //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-
     }
 }
