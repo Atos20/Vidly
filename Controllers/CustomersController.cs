946aff305460cc78318044419fd6d295b1ba5b5d
Orlando <ato.murcio@gmail.com>
2021-06-05 12:20:04 -0600

    Edit customer controller to query dtb data

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,34 +11,33 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customer
+        private MyDBContext _context;
+
+        public CustomersController()//constructor
+        {
+            _context = new MyDBContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
-            
-            return View(customers);
+            var customer = _context.Customers.ToList();
+            return View(customer);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(cust => cust.Id == id);//lambda expression is an anonymus functio.
-                                                                                 //to create one expression you  define the parameter in this case cust on the left side of the operator [=>] and on the right side od the operator and an expression or statemment block on the other side
+            var customer = _context.Customers.SingleOrDefault(cust => cust.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
 
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-            //Ienummerables is an interface that defines one method, GetEnumerator which returns Ienumerator interface. This allows read only  access to a collection then that implements IEnumerbale can be used with a for -each statement
-        {
-            return new List<Customer>
-            {
-                new Customer { Id=1, Name="Orlando Murcio"},
-                new Customer { Id=2, Name="Fabiola Murcio"}
-            };
         }
     }
 }
