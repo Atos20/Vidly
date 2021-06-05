a7a5015657b91a8efe8969b478f8e5881dbcb540
Orlando <ato.murcio@gmail.com>
2021-06-05 14:17:14 -0600

    Add Name column to database, create and siplay data

---
+++ Migrations/202106051901443_AddMermbershipTypeName.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMermbershipTypeName : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "MembershipTypeName", c => c.String());
+            AddColumn("dbo.MembershipTypes", "MembershipTypeName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "MembershipTypeName");
+            DropColumn("dbo.Customers", "MembershipTypeName");
+        }
+    }
+}
---
+++ Migrations/202106051953112_RemoveCostumerColumn.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemoveCostumerColumn : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+            DropColumn("dbo.Customers", "MembershipTypeName");
+            DropColumn("dbo.MembershipTypes", "MembershipTypeName");
+        }
+        
+        public override void Down()
+        {
+
+        }
+    }
+}
