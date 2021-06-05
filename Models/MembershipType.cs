354280cf563236bc1b4b1b0f506d4dae9ab2ccff
Orlando <ato.murcio@gmail.com>
2021-06-05 11:40:11 -0600

    Create Membership type model

---
+++ Models/MembershipType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }   //We will have just a handful of memberships //in entity frm wver editing should have an Identity
+        public short SignUpFee { get; set; } // Because we don't need values > 32000
+
+        public byte DurationsInMonth { get; set; }// the largets number will no greater that 12
+        public byte DiscountRate  { get; set; }//ranges between 0 and 100
+
+    }
+}
