88d81dfdc7513d35c33d673dfbb89fcdbfae5be6
Orlando <ato.murcio@gmail.com>
2021-06-06 06:17:14 -0600

    Update movies model to make use of Db context

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -7,21 +10,36 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private MyDBContext _context;
+
+        public MoviesController()
+        {
+            _context = new MyDBContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+
         }
+
 
         // GET: Movies/Random
         public ActionResult Random()
@@ -29,8 +47,8 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer { Id=1, Name = "Customer 1" },
-                new Customer { Id=2,  Name = "Customer 2" }
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
