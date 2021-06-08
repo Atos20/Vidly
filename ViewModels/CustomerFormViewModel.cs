9e904b8a70bd658e40febf676395de8de20363ca
Orlando <ato.murcio@gmail.com>
2021-06-08 11:14:48 -0600

    Create costumer form view model

---
+++ ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }//In the view we don't need all the functionality given by the list class so we use the IEnumerable  since the only thing we need is to iterate over the list
+
+        public Customer Customer { get; set; }
+
+    }
+}
